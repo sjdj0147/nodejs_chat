64e8526a378ebc42639f57d0bb23fc377f1bfada
sjdj0147 <86514472+sjdj0147@users.noreply.github.com>
2023-05-20 22:16:44 +0900

    실시간 채팅 제작
    nodejs와 xampp를 활용한 실시간 채팅을 만들어 봤습니다.

---
+++ index.js
@@ -0,0 +1,56 @@
+const WedSocket = require("ws");
+const ws = new WedSocket.Server({port:8008});
+
+let user_id=0 //주민동록번호처럼 클라이언트에게 부여되는 고유한 값
+let ALL_WS = [];// 전체 유저들을 통제할 수 있도록 각 유저에 대한 wevsocket, user_id 저장
+ws.on("connection", function connect(websocket,req){ //웹소켓에서 특정 클라이언트가 실행될때 실행문
+    user_id++;
+    console.log("NEW USER CONNECT ("+user_id+")");
+    ALL_WS.push({"ws":websocket, "user_id":user_id, "user_name":""});
+
+    sendUserId(user_id);
+    websocket.on("close",function close(code,reason){
+        ALL_WS.forEach(function(element, index){
+            if(element.ws == websocket){ //접속이 끊긴 유저..
+                ALL_WS.splice(index,1);
+            }
+        });
+        sendAllUsers();
+    });
+    websocket.on("message",function incoming(message){
+        console.log(JSON.parse(message));
+        message =JSON.parse(message);
+        switch(message.code){
+            case "connect_name" : //사용자 추가
+                ALL_WS.forEach(function(element, index){
+                    if(element.user_id == message.user_id){
+                        element.user_name= message.name;
+                    }
+                 });
+                sendAllUsers();
+                break;
+
+            case "send_message" : //채팅메세지 받음
+                ALL_WS.forEach(function(element, index){
+                    //element.ws 클라이언트와의 연결 지점
+                    let data={"code":"chat_message","msg":message.msg,"sender_name":message.name};
+                    element.ws.send(JSON.stringify(data));
+                 });
+                break;
+        }
+
+    });
+
+    function sendAllUsers(){ //전체 사용자 정보를 보냄
+        let data={"code":"all_users","msg":JSON.stringify(ALL_WS)};
+
+        ALL_WS.forEach(function(element, index){
+            element.ws.send(JSON.stringify(data));
+         });
+    }
+
+    function sendUserId(user_id){
+        let data = {"code":"my_user_id","msg":user_id};
+        websocket.send(JSON.stringify(data));
+    }
+});
